56527ebb75634c66d5ab602e19e6b24bd9c50510
wmtran25 <wmtran25@gmail.com>
2015-02-15 20:18:17 -0600

    Update celery_config.py

--- celery_config.py
+++ celery_config.py
@@ -10,8 +10,7 @@ if config["HEROKU_DB"]:
   import os
   url = os.environ.get('BROKER_URL')
 else:
-  url = 'redis://redistogo:735def1587e40ee9f88d1d1c270e0a33@dab.redistogo.com:9907/'
-  #url = 'redis://localhost:6379/0'
+  url = 'redis://localhost:6379/0'
 
 BROKER_URL = url
 CELERY_RESULT_BACKEND = url
