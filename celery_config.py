401419aea9c24a2885df87427469e9b6687513a7
wmtran25 <wmtran25@gmail.com>
2015-02-17 23:13:24 -0600

    Update celery_config.py

--- celery_config.py
+++ celery_config.py
@@ -6,6 +6,11 @@ CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TIMEZONE = 'US/Central'
 BROKER_POOL_LIMIT=None
 
+# Log debug messages to console
+def dlog(str):
+  if config["VERBOSE"]:
+    print 'DEBUG: %s' % str
+
 if config["HEROKU_DB"]:
   import os
   url = os.environ.get('BROKER_URL')
@@ -14,4 +19,6 @@ else:
 
 BROKER_URL = url
 CELERY_RESULT_BACKEND = url
+
+dlog('Broker_Url %s' % url)
 
